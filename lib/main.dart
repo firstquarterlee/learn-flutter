611dd83d24917fe42af26dc98c95e2027b812c99
firstquarterlee <firstquarterlee@gmail.com>
2024-05-05 19:16:25 +0900

    feat: 스크롤 구현

--- lib/main.dart
+++ lib/main.dart
@@ -6,39 +6,36 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('camera.jpg', width: 150,),
-              Container(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라팝니다.', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),),
-                    Text('강남구'),
-                    Text('7,000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        )
+        body: ListView(
+          children: [
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+          ]
+        ),
 
        )
       );
