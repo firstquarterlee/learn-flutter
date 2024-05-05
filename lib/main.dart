552bb2d3ef180c2c1930e2f23750587432b71462
firstquarterlee <firstquarterlee@gmail.com>
2024-05-05 19:39:38 +0900

    feat: 프로필 300개 증가 버튼 1개

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+MyApp({super.key});
+
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: () {
+            print(a);
+            a++;
+          },
+        ),
         appBar: AppBar(),
-        body: ListView(
-          children: [
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-            Text('안녕'),
-          ]
+        bottomNavigationBar: BottomAppBar(),
+        body: ListView.builder(
+            itemCount: 300,
+            itemBuilder: (c, i) {
+              print(i);
+              return ListTile(
+                leading: Image.asset('profile.png'),
+                title: Text('Daniel'),
+              );
+            },
         ),
 
        )
